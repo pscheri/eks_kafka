e97426d67add1aa7910a90137571b3c563facd3a
Scheri <pscheri@amazon.com>
2020-02-18 18:18:18 -0300

    .

--- eks_kafka/eks_kafka_stack.py
+++ eks_kafka/eks_kafka_stack.py
@@ -46,10 +46,13 @@ class EksKafkaStack(core.Stack):
         eks_cluster.add_capacity("worker", instance_type=ec2.InstanceType("t3.large"),
                                  min_capacity=1, max_capacity=10)
 
-        with open("service_definitions\deployment_hello-kube.json") as data:
-            deployment = json.load(data)
+        # with open("service_definitions\deployment_hello-kube.json") as data:
+        #     deployment = json.load(data)
         
-        with open("service_definitions\service_hello-kube.json") as data:
-            service = json.load(data)
+        # with open("service_definitions\service_hello-kube.json") as data:
+        #     service = json.load(data)
         
-        eks_cluster.add_resource("hello-kub", service, deployment)
+        deployment = open("service_definitions\deployment_hello-kube.json")
+        service = open("service_definitions\service_hello-kube.json")
+
+        eks_cluster.add_resource("hello-kube", json.load(service), json.load(deployment))
